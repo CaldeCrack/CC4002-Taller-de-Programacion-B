ca9f6d8ce42d4bdf89d629327d1d25089d9cf962
Nacho4756 <an.caldeguardia@gmail.com>
2023-08-22 18:01:15 -0400

    d1

--- tarea1/d1.cpp
+++ tarea1/d1.cpp
@@ -6,7 +6,7 @@ typedef pair<int, int> state;
 int main(){
     int n, x, i = 0; cin >> n >> x;
     int half_x = x/2;
-    priority_queue<state, vector<state>, greater<state>> menores;
+    priority_queue<state, vector<state>, greater<state>> menores, men_cpy;
     priority_queue<state> mayores;
     while(n--) {
         i++;
@@ -18,6 +18,7 @@ int main(){
         else
             menores.push({a, i});
     }
+    men_cpy = menores;
     while(!mayores.empty() && !menores.empty()) {
         int sum = mayores.top().first + menores.top().first;
         if(sum > x)
@@ -26,6 +27,17 @@ int main(){
             menores.pop();
         else {
             cout << mayores.top().second << " " << menores.top().second << endl;
+            return 0;
+        }
+    }
+    while(!men_cpy.empty()) {
+        state aux;
+        aux = men_cpy.top();
+        men_cpy.pop();
+        if(men_cpy.empty())
+            break;
+        if(aux.first + men_cpy.top().first == x) {
+            cout << aux.second << " " << men_cpy.top().second << endl;
             return 0;
         }
     }
