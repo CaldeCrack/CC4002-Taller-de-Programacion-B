401251ba474bdc09c5e70cbc84db03dc73e14460
Nacho4756 <an.caldeguardia@gmail.com>
2023-08-26 22:17:15 -0400

    e1 done

---
+++ tarea1/e1.cpp
@@ -0,0 +1,30 @@
+// https://vjudge.net/contest/576664#problem/E
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x, n; cin >> x >> n;
+	vector<int> positions(n);
+	for (int i = 0; i < n; i++)
+        cin >> positions[i];
+	set<int> lights{0, x};
+	for (int i : positions)
+        lights.insert(i);
+	vector<int> gaps(n);
+	int prev = 0, max_distance = 1;
+	for (int i : lights) {
+		max_distance = max(max_distance, i - prev);
+		prev = i;
+	}
+	gaps[n-1] = max_distance;
+	for (int i = n - 1; i > 0; i--) {
+		lights.erase(positions[i]);
+		auto top = lights.upper_bound(positions[i]);
+		int upper = *top;
+		int lower = *(--top);
+		max_distance = max(max_distance, upper - lower);
+		gaps[i - 1] = max_distance;
+	}
+	for (int i : gaps)
+        cout << i << ' ';
+}
