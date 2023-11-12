2a838b012808d0fe2936357ccfda8f1df994cf13
Nacho4756 <an.caldeguardia@gmail.com>
2023-11-12 17:44:44 -0300

    a ready

---
+++ tarea4/a4.cpp
@@ -0,0 +1,113 @@
+// https://vjudge.net/contest/589666#problem/A
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+template <class T, T merge(T,T)>
+struct segment_tree{
+    int N;
+    vector <T> tree;
+    segment_tree(int _N){
+        N = _N;
+        tree.resize(4*N);
+        build(0, 0, N-1);
+    }
+
+    segment_tree(vector <T> &A){
+        N = int(A.size());
+        tree.resize(4*N);
+        build(0, 0, N-1, A);
+    }
+
+    void build(int n, int i, int j){
+        if(i == j){
+        tree[n] = T(); // initial value
+        return;
+        }
+        int mid = (i+j)/2;
+        build(2*n+1, i, mid);
+        build(2*n+2, mid+1, j);
+        tree[n] = merge(tree[2*n+1], tree[2*n+2]);
+    }
+
+    void build(int n, int i, int j, vector <T> &A){
+        if(i == j){
+        tree[n] = A[i]; // initial value
+        return;
+        }
+        int mid = (i+j)/2;
+        build(2*n+1, i, mid, A);
+        build(2*n+2, mid+1, j, A);
+        tree[n] = merge(tree[2*n+1], tree[2*n+2]);
+    }
+
+    T query(int l, int r){
+        return query(0, 0, N-1, l, r);
+    }
+
+    T query(int n, int i, int j, int l, int r){
+        if(l <= i && j <= r) return tree[n];
+        int mid = (i+j)/2;
+        if(mid < l || r < i)
+        return query(2*n+2, mid+1, j, l, r);
+        if(j < l || r < mid+1)
+        return query(2*n+1, i, mid, l, r);
+        return merge(
+            query(2*n+1, i, mid, l, r),
+            query(2*n+2, mid+1, j, l, r));
+    }
+
+    void update(int t, T val){
+        update(0, 0, N-1, t, val);
+    }
+
+    void update(int n, int i, int j, int t, T val){
+        if(t < i || j < t) return;
+        if(i == j){
+        tree[n] = val;
+        return;
+        }
+        int mid = (i+j)/2;
+        update(2*n+1, i, mid, t, val);
+        update(2*n+2, mid+1, j, t, val);
+        tree[n] = merge(tree[2*n+1], tree[2*n+2]);
+    }
+
+    int search(int from, T val){
+        if(!from) return search(0, 0, N-1, val);
+        return search(0, 0, N-1, val+query(0, from-1));
+    }
+
+    int search(int n, int i, int j, T val){
+        if(tree[n] < val) return -1;
+        if(i==j && tree[n] >= val) return i;
+        int mid = (i+j)/2;
+        if(tree[2*n+1] >= val) return search(2*n+1, i, mid, val);
+        else return search(2*n+2, mid+1, j, val-tree[2*n+1]);
+    }
+};
+
+ll merge(ll a, ll b){
+    return a + b;
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    int n; cin >> n;
+    vector <ll> elems(n);
+    vector <ll> positions(n);
+    vector <ll> deleted(n, 1);
+    segment_tree <ll, merge> st(deleted);
+    for(int i = 0; i < n; i++)
+        cin >> elems[i];
+    for(int i = 0; i < n; i++)
+        cin >> positions[i];
+    for(int i = 0; i < n; i++) {
+        int k = positions[i];
+        int index = st.search(0, k);
+        cout << elems[index] << " ";
+        st.update(index, 0);
+    }
+    return 0;
+}
